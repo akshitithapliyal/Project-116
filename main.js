1c1dc9474bd292c65c5da04da5bc8cd94d8aedef
Akshiti <100438051+akshitithapliyal@users.noreply.github.com>
2022-05-20 19:57:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ function preload(){
 }
 
 function setup(){
-canvas=createCanvas(300,300);
-canvas.center();
-video=createCapture(VIDEO);
-video.size(300,300);
-video.hide();
-poseNet=ml5.postNet(video,modelLoaded);
-postNet.on('pose',gotposes);
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotposes);
 }
 
 function modelLoaded(){
